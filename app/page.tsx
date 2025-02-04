b87105d74ee4e18fb597932130454ce48b6d3373
mbilal214 <159235302+mbilal214@users.noreply.github.com>
2025-02-05 02:40:23 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import Footer from './component/Footer';
 import Header from './component/Header';
 import Link from 'next/link';
@@ -9,7 +10,7 @@ export default function Home() {
       <div className="flex flex-col items-center justify-center bg-gradient-to-r from-purple-400 to-blue-500 text-center p-8">
         <div className="w-full max-w-4xl mx-auto bg-white rounded-lg p-8 ">
           <div className="flex flex-col items-center">
-            <img src="/profile.jpg" alt="Your Name" className="w-40 h-40 rounded-full border-4 border-blue-600 mb-4 " />
+            <Image src="/profile.jpg" alt="Your Name" width={140} height={140} className=" rounded-full border-4 border-blue-600 mb-4 " />
             <h1 className="text-5xl font-bold text-gray-800">Welcome to My Portfolio</h1>
             <p className="text-gray-600 mt-2 text-lg">Frontend Developer | UI/UX Designer</p>
             <div className="mt-6 flex flex-col sm:flex-row space-y-4 sm:space-y-0 sm:space-x-4 justify-center">
@@ -22,19 +23,19 @@ export default function Home() {
             <div className="mt-4">
               {/* Project Card 1 */}
               <div className="bg-gray-100 p-6 rounded-lg shadow-md mb-8 hover:scale-105">
-                <img src="/22.jpg" alt="Cooking Website" className="w-full h-72 object-cover rounded-lg mb-2" />
+                <Image src="/22.jpg" alt="Cooking Website" width={800} height={800} className="  border border-black  rounded-lg mb-2" />
                 <h3 className="text-lg font-semibold text-gray-800">Food Recipe Template</h3>
                 <p className="text-gray-600">A beautiful template for sharing delicious food recipes.</p>
               </div>
               {/* Project Card 2 */}
               <div className="bg-gray-100 p-6 rounded-lg shadow-md mb-8 hover:scale-105">
-                <img src="/33.jpg" alt="Resume Template" className="w-full h-72 rounded-lg mb-2" />
+                <Image src="/33.jpg" alt="Resume Template" width={800} height={800} className=" border border-black rounded-lg mb-2" />
                 <h3 className="text-lg font-semibold text-gray-800">Packages</h3>
                 <p className="text-gray-600">A modern and customizable Packages template to showcase my skills.</p>
               </div>
               {/* Project Card 3 */}
               <div className="bg-gray-100 p-6 rounded-lg mb-8">
-                <img src="/44.jpg" alt="Personal Portfolio" className="w-full h-72  rounded-lg mb-2 hover:scale-105" />
+                <Image src="/44.jpg" alt="Personal Portfolio" width={800} height={800} className="  border border-black rounded-lg mb-2 hover:scale-105" />
                 <h3 className="text-lg font-semibold mt-2 text-gray-800">Personal Portfolio</h3>
                 <p className="text-gray-600">A modern and customizable Portfolio template to showcase my skills.</p>
               </div>
